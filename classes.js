385d2f37d724fe50830102aa3e5ee8b7c53849d1
Daniel Xu <daniel.xu.23@bishops.com>
2020-12-17 22:20:45 -0800

    Finished everything important (collision detection, colors, etc.)

--- classes.js
+++ classes.js
@@ -1,94 +1,445 @@
-var tankWidth = 20
-var tankLength = 32
-var tankSpeed = 10;
+var tankSpeed = 5;
 var rotateSpeed = 0.1;
-var pelletSpeed = 20;
-var pelletSize = 5;
+var turretRotateSpeed = 0.02;
+var pelletSpeed = 7;
+var pelletSize = 3;
+var health = 10;
+var tankSkinny = Math.PI/5; // Terrible variable name. This angle controls how fat the tank is.
+var tankSize = 15;
+var turretSize = 20; // More like turret length
+var ammo = 6; // Max number of pellets that can exist from one tank at a time
 
 class Tank {
-  constructor(x, y, bodyRotation, turretRotation) {
+  constructor(x, y, bodyRotation, turretRotation, color) {
     this.x = x;
     this.y = y;
     this.velX = 0;
     this.velY = 0;
     this.isMoving = false;
+    this.deceleration = 0.8;
+
+    this.color = color;
+    this.health = health;
+
     this.bodyRotation = bodyRotation;
     this.turretRotation = turretRotation;
-    this.deceleration = 0.8;
-    this.pellets = [];
+    this.bodyRotationVel = 0;
+    this.turretRotationVel = 0;
+    this.isSyncingTurretWithBody = false; // Tell if the tank should align the body to the turret
+
+    this.turretEndX = this.x + Math.cos(this.bodyRotation + this.turretRotation) * turretSize;
+    this.turretEndY = this.y - Math.sin(this.bodyRotation + this.turretRotation) * turretSize;
+
+    this.pellets = []; // List of all pellets
+
+    this.TRx = this.x + Math.cos(this.bodyRotation + tankSkinny) * tankSize;
+    this.TRy = this.y - Math.sin(this.bodyRotation + tankSkinny) * tankSize;
+    this.TLx = this.x + Math.cos(this.bodyRotation + Math.PI - tankSkinny) * tankSize;
+    this.TLy = this.y - Math.sin(this.bodyRotation + Math.PI - tankSkinny) * tankSize;
+    this.BRx = this.x + Math.cos(this.bodyRotation - tankSkinny) * tankSize;
+    this.BRy = this.y - Math.sin(this.bodyRotation - tankSkinny) * tankSize;
+    this.BLx = this.x + Math.cos(this.bodyRotation + Math.PI + tankSkinny) * tankSize;
+    this.BLy = this.y - Math.sin(this.bodyRotation + Math.PI + tankSkinny) * tankSize;
   }
   move(dir) {
+    // Moves the tank forwards or backwards (by changing velocity)
     this.isMoving = true;
     var velX = Math.cos(this.turretRotation + this.bodyRotation) * tankSpeed * dir;
     var velY = -1 * Math.sin(this.turretRotation + this.bodyRotation) * tankSpeed * dir;
     this.velX = velX
     this.velY = velY
-    if (this.turretRotation > rotateSpeed/2) {
-      this.rotateBodyRight();
-      this.rotateTurretLeft();
-    } else if (this.turretRotation < rotateSpeed/2) {
-      this.rotateBodyLeft()
-      this.rotateTurretRight();
-    }
+    this.isSyncingTurretWithBody = true;
   }
 
-  rotateBodyLeft() {
-    this.bodyRotation += rotateSpeed
-  }
-  rotateBodyRight() {
-    this.bodyRotation -= rotateSpeed
-  }
   rotateTurretLeft() {
-    this.turretRotation += rotateSpeed
+    // Rotates the turret left (by changing velocity)
+    this.turretRotationVel = turretRotateSpeed;
   }
   rotateTurretRight() {
-    this.turretRotation -= rotateSpeed
+    // Rotates the turret right (by changing velocity)
+    this.turretRotationVel = -turretRotateSpeed;
   }
   shoot(){
-    var pellVelX = Math.cos(this.turretRotation + this.bodyRotation) * pelletSpeed;
-    var pellVelY = -1 * Math.sin(this.turretRotation + this.bodyRotation) * pelletSpeed;
-    var pellet = new Pellet(this.x, this.y, pellVelX, pellVelY)
-    this.pellets.push(pellet);
+    // Shoots a pellet
+    if (this.pellets.length < ammo) {
+      // Determine the velocity of the pellet
+      var pellVelX = Math.cos(this.turretRotation + this.bodyRotation) * pelletSpeed;
+      var pellVelY = -1 * Math.sin(this.turretRotation + this.bodyRotation) * pelletSpeed;
+      // Create a moving pellet
+      var pellet = new Pellet(this.turretEndX, this.turretEndY, pellVelX, pellVelY, this.color)
+      this.pellets.push(pellet);
+    }
   }
   draw(){
+    /*
+      This method is regularly called, similar to the drawAll() function.
+      Changes positions of the tank and draws it.
+    */
+
+    // Change the position of the tank
     if (this.isMoving) {
+      // Move the tank at full speed
       this.x += this.velX;
       this.y += this.velY;
     } else if (Math.sqrt(this.velX**2 + this.velY**2) > 0.1) {
+      // Decelerate the tank
       this.velX *= this.deceleration;
       this.velY *= this.deceleration;
       this.x += this.velX;
       this.y += this.velY;
     }
 
+    // Align the body with the turret after moving
+    if (this.isSyncingTurretWithBody) {
+      if (this.turretRotation > rotateSpeed/2) {
+        this.bodyRotationVel = rotateSpeed;
+      } else if (this.turretRotation < - rotateSpeed/2) {
+        this.bodyRotationVel = -rotateSpeed;
+      } else {
+        this.bodyRotationVel = 0;
+        this.isSyncingTurretWithBody = false;
+      }
+    }
+
+    // Rotate the tank body and the turret
+    this.bodyRotation += this.bodyRotationVel;
+    this.bodyRotation = adjustAngles(this.bodyRotation);
+    this.turretRotation -= this.bodyRotationVel;
+    this.turretRotation += this.turretRotationVel;
+    this.turretRotation = adjustAngles(this.turretRotation);
+
+    // Update coordinates of tank corners
+    this.TRx = this.x + Math.cos(this.bodyRotation + tankSkinny) * tankSize;
+    this.TRy = this.y - Math.sin(this.bodyRotation + tankSkinny) * tankSize;
+    this.TLx = this.x + Math.cos(this.bodyRotation + Math.PI - tankSkinny) * tankSize;
+    this.TLy = this.y - Math.sin(this.bodyRotation + Math.PI - tankSkinny) * tankSize;
+    this.BRx = this.x + Math.cos(this.bodyRotation - tankSkinny) * tankSize;
+    this.BRy = this.y - Math.sin(this.bodyRotation - tankSkinny) * tankSize;
+    this.BLx = this.x + Math.cos(this.bodyRotation + Math.PI + tankSkinny) * tankSize;
+    this.BLy = this.y - Math.sin(this.bodyRotation + Math.PI + tankSkinny) * tankSize;
+
+    // Update coordinates of the end of the turret
+    this.turretEndX = this.x + Math.cos(this.bodyRotation + this.turretRotation) * turretSize;
+    this.turretEndY = this.y - Math.sin(this.bodyRotation + this.turretRotation) * turretSize;
+
+    // Regeneration
+    this.health += 0.001;
+
+    // Draw pellets
     context.beginPath();
-    // context.translate(-1 * this.x, -1 * this.y);
-    // context.rotate(this.bodyRotation);
     for (var i = 0; i < this.pellets.length; i++) {
       this.pellets[i].draw();
     }
-    context.rect(this.x-tankWidth/2, this.y - tankLength/2, tankWidth, tankLength);
 
-    // context.rotate(-1 * this.bodyRotation);
-    // context.translate(this.x, this.y);
+    // Draw tank
+    context.beginPath();
+    context.strokeStyle = this.color;
+    drawLine(this.TRx, this.TRy, this.TLx, this.TLy);
+    drawLine(this.TLx, this.TLy, this.BLx, this.BLy);
+    drawLine(this.BLx, this.BLy, this.BRx, this.BRy);
+    drawLine(this.BRx, this.BRy, this.TRx, this.TRy);
     context.stroke();
-    // TODO:
+
+    // Draw turret
+    context.beginPath();
+    context.lineWidth = 3;
+    drawLine(this.x, this.y, this.turretEndX, this.turretEndY);
+    context.stroke();
+    context.lineWidth = 1;
+    context.strokeStyle = "black";
+
+    // Draw health bar
+    var startX = this.TRx/4 + this.BRx/4 + this.x/2;
+    var startY = this.TRy/4 + this.BRy/4 + this.y/2;
+    var endX = this.TLx/4 + this.BLx/4 + this.x/2;
+    var endY = this.TLy/4 + this.BLy/4 + this.y/2;
+    var fracHealth = this.health / health;
+    // Red part
+    context.beginPath();
+    context.lineWidth = 8;
+    context.strokeStyle = "red";
+    drawLine(startX, startY, fracHealth*startX + (1-fracHealth)*endX, fracHealth*startY + (1-fracHealth)*endY);
+    context.stroke();
+    // Green part
+    context.beginPath();
+    context.strokeStyle = "green";
+    drawLine(fracHealth*startX + (1-fracHealth)*endX, fracHealth*startY + (1-fracHealth)*endY, endX, endY);
+    context.stroke();
+
+    // Reset context properties
+    context.lineWidth = 1;
+    context.strokeStyle = "black";
+
   }
+  nextFrame(){
+    // Returns a copy of the tank one frame ahead. This is usefull in collision detection.
+    return new Tank(this.x + this.velX, this.y + this.velY, this.bodyRotation, 0);
+  }
+
+  contains(x, y){
+    // Determines if point (x,y) is inside the tank.
+
+    // Shift the point so that the center of the tank (this.x, this.y) is at the origin.
+    var adjustedX = x - this.x;
+    var adjustedY = y - this.y;
+    // Define and apply a rotation matrix:
+    //   a  c
+    //   b  d
+    var a = Math.cos(-this.bodyRotation);
+    var b = Math.sin(-this.bodyRotation);
+    var c = Math.sin(this.bodyRotation);
+    var d = Math.cos(this.bodyRotation);
+
+    var newX = a*adjustedX + c * adjustedY;
+    var newY = b*adjustedX + d * adjustedY;
+
+    // Now that everything has been rotated so that the tank's edges
+    // are parallel to the x and y axes, the job is easy.
+    if (Math.abs(newX) <= Math.cos(tankSkinny) * tankSize && Math.abs(newY) <= Math.sin(tankSkinny) * tankSize) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 }
 
 class Pellet {
-  constructor(x, y, velX, velY) {
+  constructor(x, y, velX, velY, color) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+    this.color = color;
   }
   draw(){
     this.x += this.velX;
     this.y += this.velY;
 
     context.beginPath();
+    context.strokeStyle = this.color;
     context.arc(this.x, this.y, pelletSize, 0, 2 * Math.PI);
     context.stroke();
+    context.strokeStyle = "black";
   }
+}
+
+class Wall {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.TLx = x;
+    this.TLy = y;
+    this.BLx = x;
+    this.BLy = y + height;
+    this.TRx = x + width;
+    this.TRy = y;
+    this.BRx = x + width;
+    this.BRy = y + height;
+  }
+  contains(x,y){
+    // Determine if a point (x,y) is in the wall.
+    if (this.x <= x && x <= this.x + this.width && this.y <= y && y <= this.y + this.height) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  draw(){
+    // context.beginPath();
+    context.fillStyle = "#A0A0A0";
+    context.fillRect(this.x, this.y, this.width, this.height);
+    // context.rect(this.x, this.y, this.width, this.height);
+    // context.stroke();
+  }
+}
+
+function drawLine(x1, y1, x2, y2){
+  // Easy way to draw a line from (x_1,y_1) to (x_2, y_2).
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.stroke();
+}
+
+function adjustAngles(angle){
+  // Adjusts every angle to be between -π and π.
+  while (angle > Math.PI) {
+    angle -= 2*Math.PI
+  }
+  while (angle < -Math.PI) {
+    angle += 2*Math.PI
+  }
+  return angle;
+}
+
+function detectCollisions(x, y, item){
+  // Detect if a collision has occurrred between point (x,y) and an item.
+  if (item.contains(x, y)){
+    return true;
+  }
+  return false;
+}
+
+function tankToTankInteractions(tankA, tankB){
+  /*
+    Checks for two things
+      1. Collision between tanks
+      2. The pellets of one tank hitting the other
+  */
+
+  // Check if tankA pellets hit tank B.
+  tankA.pellets.forEach((pellet) => {
+    if (tankB.contains(pellet.x, pellet.y) || tankB.contains(pellet.x + pelletSize, pellet.y) || tankB.contains(pellet.x + pelletSize, pellet.y + pelletSize) || tankB.contains(pellet.x, pellet.y + pelletSize)) {
+      // Pellets hit tank B
+      var index = tankA.pellets.indexOf(pellet);
+      if (index > -1) {
+        // Delete the pellet
+        tankA.pellets.splice(index, 1);
+      }
+      // Decrease tank B's health
+      tankB.health -= 1;
+      // Check if tank B is dead
+      if (tankB.health <= 0) {
+        LOSER = tankB.color;
+      }
+    }
+  });
+
+  // Check if tank B contains the corners of tank A.
+  var collision = false;
+  if (tankB.nextFrame().contains(tankA.TLx, tankA.TLy)) {
+    collision = true;
+  }
+  if (tankB.nextFrame().contains(tankA.TRx, tankA.TRy)) {
+    collision = true;
+  }
+  if (tankB.nextFrame().contains(tankA.BLx, tankA.BLy)) {
+    collision = true;
+  }
+  if (tankB.nextFrame().contains(tankA.BRx, tankA.BRy)) {
+    collision = true;
+  }
+  if (collision) {
+    // There is a collision
+    // Stop tank B from moving.
+    tankB.velX = 0;
+    tankB.velY = 0;
+
+    // Collision damage
+    tankB.health -= 0.05;
+    // Check if tank B is dead
+    if (tankB.health <= 0) {
+      LOSER = tankB.color;
+    }
+  }
+}
+
+function tankInWallCollisions(tank, wall){
+  // Determine if a corner of the tank is in the wall. If yes, stop the tank.
+  if(detectCollisions(tank.nextFrame().TLx, tank.nextFrame().TLy, wall)){
+    tank.velX = 0;
+    tank.velY = 0;
+  };
+  if(detectCollisions(tank.nextFrame().TRx, tank.nextFrame().TRy, wall)){
+    tank.velX = 0;
+    tank.velY = 0;
+  };
+  if(detectCollisions(tank.nextFrame().BLx, tank.nextFrame().BLy, wall)){
+    tank.velX = 0;
+    tank.velY = 0;
+  };
+  if(detectCollisions(tank.nextFrame().BRx, tank.nextFrame().BRy, wall)){
+    tank.velX = 0;
+    tank.velY = 0;
+  };
+}
+
+function wallInTankCollisions(tank, wall){
+  // Determine if a corner of the wall is in the tank. If yes, stop the tank.
+  if(detectCollisions(wall.TRx, wall.TRy, tank.nextFrame())){
+    tank.velX = 0;
+    tank.velY = 0;
+  };
+  if(detectCollisions(wall.TLx, wall.TLy, tank.nextFrame())){
+    tank.velX = 0;
+    tank.velY = 0;
+  };
+  if(detectCollisions(wall.BRx, wall.BRy, tank.nextFrame())){
+    tank.velX = 0;
+    tank.velY = 0;
+  };
+  if(detectCollisions(wall.BLx, wall.BLy, tank.nextFrame())){
+    tank.velX = 0;
+    tank.velY = 0;
+  };
+}
+
+function wallAndPelletCollisions(tank, wall){
+  // Determine if any of a tank's pellets are in the wall.
+  tank.pellets.forEach((pellet) => {
+    var collision = false;
+    if(detectCollisions(pellet.x, pellet.y, wall)){
+      collision = true;
+    }
+    if(detectCollisions(pellet.x + pelletSize, pellet.y, wall)){
+      collision = true;
+    }
+    if(detectCollisions(pellet.x - pelletSize, pellet.y, wall)){
+      collision = true;
+    }
+    if(detectCollisions(pellet.x, pellet.y + pelletSize, wall)){
+      collision = true;
+    }
+    if(detectCollisions(pellet.x, pellet.y - pelletSize, wall)){
+      collision = true;
+    }
+    if(detectCollisions(pellet.x + pelletSize, pellet.y + pelletSize, wall)){
+      collision = true;
+    }
+    if(detectCollisions(pellet.x - pelletSize, pellet.y - pelletSize, wall)){
+      collision = true;
+    }
+    if(detectCollisions(pellet.x + pelletSize, pellet.y + pelletSize, wall)){
+      collision = true;
+    }
+    if(detectCollisions(pellet.x - pelletSize, pellet.y - pelletSize, wall)){
+      collision = true;
+    }
+
+    if (collision) {
+      // A pellet hit the wall. Delete it.
+      var index = tank.pellets.indexOf(pellet);
+      if (index > -1) {
+        tank.pellets.splice(index, 1);
+      }
+    }
+  });
+}
+
+function fillBackground(){
+  // This function draws the background.
+
+  // Fills the background with a lovely solid gray color.
+  context.fillStyle = "#FCFCFC";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  context.beginPath();
+
+  // Draw the vertical grid lines
+  var i=0;
+  while (i < canvas.width) {
+    context.strokeStyle = "#DDDDDD";
+    drawLine(i, 0, i, canvas.height);
+    i += 100;
+  }
+
+  // Draw the horizontal grid lines
+  i=0;
+  while (i < canvas.height) {
+    drawLine(0, i, canvas.width, i);
+    i+= 100;
+  }
+  context.stroke();
 }
--- newGame.js
+++ newGame.js
@@ -1,11 +1,44 @@
-function drawAll(){
-  context.clearRect(0,0,canvas.width, canvas.height);
+// TODO: restart option, add powerups, wall health?
 
+function drawAll(){
+  /*
+    This is the main function that loops and draws everything.
+  */
+
+  // Check for Wall + Tank or Wall + Pellet collisions
+  tanks.forEach((tank) => {
+    walls.forEach((wall) => {
+      // Check to see if a corner of a tank is in a wall
+      tankInWallCollisions(tank,wall);
+
+      // Check to see if a corner of a wall is in a tank
+      wallInTankCollisions(tank, wall);
+
+      // Check to see if a pellet is in a wall
+      wallAndPelletCollisions(tank, wall);
+    });
+  });
+
+  // Account for collisions between tanks, and pellets hitting tanks
+  tankToTankInteractions(tank1, tank2);
+  tankToTankInteractions(tank2, tank1);
+
+  // Drawing Everything
+  context.clearRect(0,0,canvas.width, canvas.height);
   context.beginPath();
-  // context.arc(circlePos[0], circlePos[1], 50, 0, 2 * Math.PI);
-  context.stroke();
-  TANK.draw();
-  window.requestAnimationFrame(drawAll)
+  fillBackground();
+  tank1.draw();
+  tank2.draw();
+  walls.forEach((wall) => {
+    wall.draw();
+  });
+
+  // Check if game over
+  if (LOSER == "none") {
+    window.requestAnimationFrame(drawAll)
+  } else {
+    gameOver(LOSER);
+  }
 }
 
 // Get width/height of the browser window
@@ -15,7 +48,6 @@ console.log("Window is %d by %d", windowWidth, windowHeight);
 
 // Get the canvas, set the width and height from the window
 canvas = document.getElementById("canvas");
-// I found that - 20 worked well for me, YMMV
 canvas.width = windowWidth - 20;
 canvas.height = windowHeight - 20;
 canvas.style.border = "1px solid black";
@@ -23,16 +55,67 @@ canvas.style.border = "1px solid black";
 // Set up the context for the animation
 context = canvas.getContext("2d");
 
+// Set up event listeners to check if a key is pressed or released
 document.addEventListener("keyup", myKeyUp)
 document.addEventListener("keydown", myKeyDown);
 
-var TANK = new Tank(100, 100, 0, 0);
-console.log(TANK);
+// Changes to the name of the tank that loses its health.
+// This variable tells us when the game is over.
+var LOSER = "none";
 
+// Tells if a key is pressed. Used for game over screen.
+// var keyPressed = false;
+
+// Create the tanks
+var tank1 = new Tank(100, 100, 0, 0, "blue");
+var tank2 = new Tank(800, 100, 0, 0, "red");
+var tanks = [tank1, tank2];
+
+// Create the walls
+var walls = [];
+
+// Four borders of the canvas
+walls.push(new Wall(-50, 0, 50, canvas.height));
+walls.push(new Wall(canvas.width, 0, 50, canvas.height));
+walls.push(new Wall(0, -50, canvas.width, 50));
+walls.push(new Wall(0, canvas.height, canvas.width, 50));
+
+// "Actual" walls
+walls.push(new Wall(200, 0, 300, 400));
+walls.push(new Wall(300, 500, 900, 100));
+walls.push(new Wall(1200, 400, 100, 300));
+walls.push(new Wall(1000, 100, 200, 100));
+
+// Starts the game
 window.requestAnimationFrame(drawAll)
 
 function myKeyUp (event) {
-  TANK.isMoving = false;
+  /*
+    Parameters: event object, which contains information about the event
+      that triggered the event listener.
+    Returns: None, but modifies global variables which track response to event.
+    Purpose: Make the animation respond to keys being released.
+  */
+  keyStr = event.key;
+  keyCode = event.which;
+  if (keyStr == 'w' || keyStr == 's') {
+    // Tank 1 stopped moving forward
+    tank1.isMoving = false;
+  }
+  if (keyStr == 'a' || keyStr == 'd') {
+    // Tank 1 stopped rotating
+    tank1.turretRotationVel = 0;
+  }
+  if (keyCode == '38' || keyCode == '40') {
+    // Tank 2 stopped moving forward
+    tank2.isMoving = false;
+  }
+  if (keyCode == '37' || keyCode == '39') {
+    // Tank 2 stopped rotating
+    tank2.turretRotationVel = 0;
+  }
+
+  // keyPressed = false;
 }
 
 function myKeyDown (event) {
@@ -42,43 +125,75 @@ function myKeyDown (event) {
     Returns: None, but modifies global variables which track response to event.
     Purpose: Make the animation respond to keys being pressed.
   */
-  // One of the attributes of the event object is 'which,' contains the key
-  //   that was pressed to trigger the event listener.
-  TANK_MOVING = true;
   keyCode = event.which;
   keyStr = event.key;
-  console.log(event);
-  console.log(keyCode);
-  console.log(keyStr);
 
+  if (keyStr == 'e') {
+    // Tank 1 shoots
+    tank1.shoot();
+  }
   if (keyStr == 'w') {
-    // Move tank forward
-    TANK.move(1);
+    // Tank 1 moves forwards
+    tank1.move(1);
   }
   if (keyStr == 's') {
-    // Move tank backwards
-    TANK.move(-1);
+    // Tank 1 moves backwards
+    tank1.move(-1);
   }
   if (keyStr == 'a') {
-    TANK.rotateBodyLeft();
+    // Tank 1 rotates left
+    tank1.rotateTurretLeft();
   }
   if (keyStr == 'd') {
-    TANK.rotateBodyRight();
+    // Tank 1 rotates right
+    tank1.rotateTurretRight();
   }
-  if (keyStr == 'e') {
-    TANK.shoot();
+
+  if (keyStr == '/') {
+    // Tank 2 shoots
+    tank2.shoot();
   }
+  if (keyCode == '38') {
+    // Tank 2 moves forwards
+    tank2.move(1);
+  }
+  if (keyCode == '40') {
+    // Tank 2 moves backwards
+    tank2.move(-1);
+  }
+  if (keyCode == '37') {
+    // Tank 2 rotates left
+    tank2.rotateTurretLeft();
+  }
+  if (keyCode == '39') {
+    // Tank 2 rotates right
+    tank2.rotateTurretRight();
+  }
+
+  // keyPressed = true;
 }
 
-function drawRect(centerX, centerY, angle){
-  context.beginPath();
-  x=0;
-  y=0;
-  context.rect(centerX - x, centerY - y, tankWidth, tankHeight)
-  context.translate(centerX, centerY)
-  context.rotate(angle);
+function gameOver(LOSER){
+  /*
+    Parameters: Name of the tank that lost all its health (red or blue).
+    Returns: None. This ends the game.
+    Purpose: Display the graphics to say that the game is over.
+  */
 
-  context.rotate(-1 * angle);
-  context.translate(-1 * centerX, -1 * centerY)
-  context.stroke();
+  // Determine the winner
+  var WINNER;
+  if (LOSER == "blue") {
+    WINNER = "red";
+  } else {
+    WINNER = "blue";
+  }
+
+  // Write the ending message
+  context.font = "30px Helvetica";
+  context.fillStyle = "black";
+  context.textAlign = "center";
+  context.fillText("GAME OVER", canvas.width/2, canvas.height/2);
+  context.font = "20px Helvetica";
+  context.fillText(WINNER.toUpperCase() + " won", canvas.width/2, canvas.height/2 + 50);
+  context.fillText("Press any key to continue", canvas.width/2, canvas.height/2 + 100);
 }
